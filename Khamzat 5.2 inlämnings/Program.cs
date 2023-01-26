d4ca5f5fb5b2f705951da9c91b5f93090b1747f3
Pingvu <nhlpool27@gmail.com>
2023-01-26 13:57:26 +0100

    1

--- Khamzat 5.2 inlämnings/Program.cs
+++ Khamzat 5.2 inlämnings/Program.cs
@@ -1,32 +1,42 @@
 ﻿using System;
 
-namespace Uppgift_5._
+namespace Uppgift_5._2_khamzat
 {
     class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
 
-            int[] tal = new int[3];
+                int[] tal = new int[3];
 
-            tal[0] = 2;
-            tal[1] = 4;
+                tal[0] = 2;
+                tal[1] = 4;
+
+                {
+                    Console.WriteLine("den Första talet " + tal[0]);
+                    Console.WriteLine("den Andra tal " + tal[1]);
+                    Console.WriteLine("Skriv in det tredje talet");
+                }
+                tal[2] = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Alla tal och den du skrev är {tal[0]}, {tal[1]}, {tal[2]}");
+
+                Console.ReadKey();
+            }
+            catch
 
             {
-                Console.WriteLine("den Första talet " + tal[0]);
-                Console.WriteLine("den Andra tal " + tal[1]);
-                Console.WriteLine("Skriv in det tredje talet");
+                Console.WriteLine("du skrev inte heltal");
+                Console.ReadKey();
             }
-            tal[2] = int.Parse(Console.ReadLine());
-
-            Console.WriteLine($"Alla tal och den du skrev är {tal[0]}, {tal[1]}, {tal[2]}");
 
 
         }
-
     }
-}
 
+}
 
 
 
